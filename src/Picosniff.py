52438176bba177ef1830d18ba1cb81a40d61191a
RhysMarch <c1012902@newcastle.ac.uk>
2024-03-11 20:46:45 +0000

    ASCII Art credits and modification

--- src/Picosniff.py
+++ src/Picosniff.py
@@ -2,13 +2,17 @@ from packet_sniffer import *
 
 
 def picosniff_ascii():
+    # ASCII Art generated using Patorjk's Text to ASCII Art Generator
+    # Source: https://patorjk.com/software/taag/
+    # Font: Slant
     print("""\
         
-       ___  _          ____     _ ______
-      / _ \(_)______  / __/__  (_) _/ _/
-     / ___/ / __/ _ \_\ \/ _ \/ / _/ _/ 
-    /_/  /_/\__/\___/___/_//_/_/_//_/   
-                                    
+        ____  _                       _ ________
+       / __ \(_)________  _________  (_) __/ __/
+      / /_/ / / ___/ __ \/ ___/ __ \/ / /_/ /_  
+     / ____/ / /__/ /_/ (__  ) / / / / __/ __/  
+    /_/   /_/\___/\____/____/_/ /_/_/_/ /_/     
+                                                                    
     """)
 
 
