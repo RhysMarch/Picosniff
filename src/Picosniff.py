133460a2c587e373bbb6cc8ed85cb631bb62e323
RhysMarch <c1012902@newcastle.ac.uk>
2024-04-14 00:41:28 +0200

    Picosniff.py +docstring

--- src/Picosniff.py
+++ src/Picosniff.py
@@ -1,3 +1,15 @@
+"""
+This script, Picosniff.py, serves as the main entry point for the Picosniff application. It initializes
+and runs the packet sniffing and CLI handling functionalities.
+
+Functions:
+- picosniff_ascii(): Prints the Picosniff ASCII art logo.
+- main(): Initializes the PacketSniffer and CLIHandler classes and handles the main application loop.
+
+The script is intended to be run directly from the command line and interacts with the user to manage
+packet sniffing operations.
+"""
+
 from packet_sniffer import PacketSniffer
 from cli_handler import CLIHandler
 
