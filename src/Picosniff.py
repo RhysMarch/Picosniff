06ef76a43b70a63baf1531c8caee6a7ac2d0b587
RhysMarch <c1012902@newcastle.ac.uk>
2024-04-18 09:15:46 +0100

    ASCII art moved to utils.py file

--- src/Picosniff.py
+++ src/Picosniff.py
@@ -12,21 +12,7 @@ packet sniffing operations.
 
 from packet_sniffer import PacketSniffer
 from cli_handler import CLIHandler
-
-
-def picosniff_ascii():
-    # ASCII Art generated using Patorjk's Text to ASCII Art Generator
-    # Source: https://patorjk.com/software/taag/
-    # Font: Slant
-    print("""\
-                  
-                    ____  _                       _ ________
-                   / __ \(_)________  _________  (_) __/ __/
-                  / /_/ / / ___/ __ \/ ___/ __ \/ / /_/ /_  
-                 / ____/ / /__/ /_/ (__  ) / / / / __/ __/  
-                /_/   /_/\___/\____/____/_/ /_/_/_/ /_/        
-                                                                     
-    """)
+from utils import picosniff_ascii
 
 
 def main():
