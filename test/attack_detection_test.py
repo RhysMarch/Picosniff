e5b0ba4484f996a5a70f76823bf3ff2971b8d1c1
RhysMarch <c1012902@newcastle.ac.uk>
2024-05-09 11:15:07 +0100

    Revert "Linux tests"
    This reverts commit c338398a5b612ec5610ac1bfe9ba445a4698f49c.

--- test/attack_detection_test.py
+++ test/attack_detection_test.py
@@ -1,4 +1,3 @@
-import socket
 from random import randint
 from scapy.layers.dns import DNS, DNSQR
 from scapy.layers.inet import IP, TCP, UDP
@@ -24,47 +23,6 @@ def simulate_syn_flood(interface, victim_ip=None, target_port=80, count=250, pac
 
     for _ in range(count):
         send(packet, iface=interface, verbose=0)
-
-
-def simulate_syn_flood_linux(interface, victim_ip=None, target_port=80, count=250, packet_size=1024):
-    if not victim_ip:
-        victim_ip = get_local_ip()
-
-    s = socket.socket(socket.AF_PACKET, socket.SOCK_RAW)
-    s.bind((interface, 0))
-
-    ip_layer = IP(dst=victim_ip)
-    tcp_layer = TCP(sport=RandShort(), dport=target_port, flags="S")
-    raw_payload = Raw(b"SYN_Flood_" + b"X" * (packet_size - len(b"SYN_Flood_")))
-
-    packet = ip_layer / tcp_layer / raw_payload
-    for _ in range(count):
-        s.send(bytes(packet))
-
-    s.close()
-
-
-def simulate_dns_flood_linux(interface, victim_ip=None, target_port=53, count=500):
-    if not victim_ip:
-        victim_ip = get_local_ip()
-
-    # Create a raw socket and bind it to the specified interface
-    s = socket.socket(socket.AF_PACKET, socket.SOCK_RAW)
-    s.bind((interface, 0))
-
-    for _ in range(count):
-        # Randomize the source port and generate a random subdomain for the DNS query
-        udp_layer = UDP(sport=RandShort(), dport=target_port)
-        subdomain = RandString(12).decode() + ".example.com"
-        dns_layer = DNS(rd=1, qd=DNSQR(qname=subdomain))
-
-        # Construct the packet
-        packet = IP(dst=victim_ip) / udp_layer / dns_layer
-
-        # Send the packet through the raw socket
-        s.send(bytes(packet))
-
-    s.close()
 
 
 def simulate_dns_flood(interface, victim_ip=None, target_port=53, count=500):
